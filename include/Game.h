7142306f449b36b7f1fd60c49bff8ffe4da1a125
Kewei Han <hankevin@umich.edu>
2024-01-04 18:32:47 -0500

    Design progress

--- include/Game.h
+++ include/Game.h
@@ -10,11 +10,53 @@
 
 namespace SimpleECS
 {
+	/**
+	* A game instance represents the main game controller.
+	* Sets up initialization and runs the main game loop. 
+	*/
 	class Game {
 	public:
-		SIMPLEECS_API void StartGame();
-		
+		/**
+		 * Game constructor. Creates an empty game.
+		 *
+		 */
+		SIMPLEECS_API Game();
+
+		/**
+		 * Start the main game loop. Game must have at least one scene.
+		 *
+		 * @throws Exception: If no scenes exist in game
+		 */
+		SIMPLEECS_API void startGame();
+
+		/**
+		 * Adds a scene containing entities to game instance.
+		 *
+		 * @param scene: Scene to be added.
+		 */
+		SIMPLEECS_API int addScene(Scene scene);
+
 	private:
-		std::vector<Scene> scene;
+		std::vector<Scene> sceneList;
+
+		SDL_Window* window			= nullptr;
+		SDL_Surface* screenSurface	= nullptr;
+
+		const int SCREEN_WIDTH	= 640;
+		const int SCREEN_HEIGHT = 480;
+
+		/**
+		 * Call initialization functions
+		 * 
+		 * @returns 0 on failure, 1 on success.
+		 * @throws Exception: On failure
+		 */
+		void init();
+
+		/**
+		 * Constructs a window displaying current scene that closes on 'x'.
+		 * Runs Component functions. 
+		 */
+		void mainLoop();
 	};
 }
--- src/Game.cpp
+++ src/Game.cpp
@@ -4,45 +4,48 @@
 
 using namespace SimpleECS;
 
-void Game::StartGame()
+void Game::startGame()
 {
-	std::cout << "Game also says hello" << std::endl;
-	
-	const int SCREEN_WIDTH = 640;
-	const int SCREEN_HEIGHT = 480;
-
-	SDL_Window* window = NULL;
-	SDL_Surface* screenSurface = NULL;
-
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
-		{
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			// Set white window surface
-			screenSurface = SDL_GetWindowSurface(window);
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-			SDL_UpdateWindowSurface(window);
-
-			// TempLoop
-			SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
-		}
-	}
-
-	//Destroy window
-	SDL_DestroyWindow(window);
-
-	//Quit SDL subsystems
-	SDL_Quit();
+	init();
+	mainLoop();
 }
 
+int Game::addScene(Scene scene)
+{
+	sceneList.push_back(scene);
+	return static_cast<int>(sceneList.size() - 1);
+}
+
+void Game::init()
+{
+	// Initialize SDL
+	if (SDL_InitSubSystem(SDL_INIT_VIDEO))
+	{
+		throw std::runtime_error("Failure to initialize SDL. SDL_Error: " + std::string(SDL_GetError()));
+	}
+	
+	// Create window
+	window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (window == NULL)
+	{
+		throw std::runtime_error("Failure to create window. SDL_Error: " + std::string(SDL_GetError()));
+	}
+}
+
+void Game::mainLoop()
+{
+	SDL_Event e;
+	bool quit = false;
+
+	// Game loop
+	while (!quit)
+	{
+		while (SDL_PollEvent(&e))
+		{
+			if (e.type == SDL_QUIT)
+			{
+				quit = true;
+			}
+		}
+	}
+}
